3e5d199b1628ede4f38f51f88eefb5d0958a93a1
Jong Shin <jshin.m@gmail.com>
2022-07-06 07:07:35 -0400

    Update version for package building (#13)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Pandarize',
-    version='0.0.1',
+    version='0.0.2',
     author='Jong M. Shin',
     author_email='jshinm@gmail.com',
     packages=['pandarize'],
