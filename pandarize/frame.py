aad6a3f6f03d36b1b28a5c9d7cb37e8801a8ce56
Jong Shin <jshin.m@gmail.com>
2022-07-11 19:52:34 -0400

    Simplify `Load()` method (#31)

--- pandarize/frame.py
+++ pandarize/frame.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from pandarize import _util
+from pandarize._util import *
 
 class Pandarizer:
     def __init__(self):
@@ -7,23 +7,19 @@ class Pandarizer:
         self.df = None
 
     def load(self, source=None, savefile=None):
-        '''Loads data from either local file or the url
+        '''Loads raw data from either local file or the url
         '''
-        if _util.check_url(string=source):
-            self.raw = _util.url_loader(url=source, savefile=savefile)
-        else:
-            self.df = pd.read_excel(source, dtype=str)
-            self.df.fillna('', inplace=True)
+        self.raw = source_loader(source=source, savefile=savefile)
 
     def fit(self, kind='bib'):
         '''Method that infers data structure (in the future)
         '''
         if kind == 'bib':
-            self.df = _util.bib_parser(raw=self.raw)
+            self.df = bib_parser(raw=self.raw)
 
     def transform(self, formats='bib', types=None, alias=None, dirs=None):
         '''Transform loaded data into a specified data type
         '''
 
         if formats == 'bib':
-            _util.bib_writer(df=self.df, types=types, alias=alias, dirs=dirs)
+            bib_writer(df=self.df, types=types, alias=alias, dirs=dirs)
