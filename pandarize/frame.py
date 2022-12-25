b2aaf3afa2d7e6c94ffb76251be83fb3c8cd9935
Jong Shin <jshin.m@gmail.com>
2022-12-25 07:36:37 -0500

    Add basic stdout for metadata (#50)

--- pandarize/frame.py
+++ pandarize/frame.py
@@ -25,3 +25,19 @@ class Pandarizer:
         '''
         if formats == 'bib':
             bib_writer(df=self.df, types=types, alias=alias, dirs=dirs)
+            
+    def describe(self):
+        '''Reports basic metadata'''
+        
+        if not self.df:
+            print('No file is loaded. Please load() and fit() to retrieve metadata.')
+            return 0
+            
+        if self.df.shape[0] == 0 or self.df.shape[1] == 0:
+            print('The file has not been loaded successfully. Please check the file path and/or make sure that file is not corrupted.')
+            return 0
+            
+        print(f'''The loaded file has {df.shape[0]} rows and {df.shape[1]} columns.\n
+              ''')
+        
+        ## Config Setting
