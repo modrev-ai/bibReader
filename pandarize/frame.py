e13e128c2fa44db17fb1bb114f9da126924c6872
Jong Shin <jshin.m@gmail.com>
2022-12-30 22:50:53 -0500

    Refactor code to use other modules (#52)

--- pandarize/frame.py
+++ pandarize/frame.py
@@ -1,40 +1,37 @@
 import pandas as pd
-from pandarize._util import *
+from ._util import *
+from .loader import Loader
+from .parser import Parser
 
-
-class Pandarizer:
+class Pandarizer(Loader, Parser):
     def __init__(self):
-        self.raw = None
-        self.df = None
         self.idxkey = None
-        self.settings = None
-        
         
     def initialize(self, yaml=False, path=None):
         '''Initializes the setting either for the first time by
         loading a default yaml config file in system dir or 
         load from an user-specified existing the file in `path`
         '''
-        self.settings = load_config(yaml=yaml, path=path)
+        self.load_config(yaml=yaml, path=path)
 
     def load(self, source=None, savefile=None):
         '''Loads raw data from either local file or the url
         '''
-        self.raw = source_loader(source=source, savefile=savefile)
-        self.raw = bib_preprocessing(raw=self.raw)
+        self.source_loader(source=source, savefile=savefile)
+        self.bib_preprocessing()
         self.idxkey = rfindall_matched(self.raw, r'[.*]?@[^}]*{*[,]', '@')
 
     def fit(self, kind='bib', postprocess=False):
         '''Method that infers data structure (in the future)
         '''
         if kind == 'bib':
-            self.df = bib_parser(raw=self.raw, idxkey=self.idxkey, postprocess=postprocess)
+            self.bib_parser(raw=self.raw, idxkey=self.idxkey, postprocess=postprocess)
 
     def transform(self, formats='bib', types=None, alias=None, dirs=None):
         '''Transform loaded data into a specified data type
         '''
         if formats == 'bib':
-            bib_writer(df=self.df, types=types, alias=alias, dirs=dirs)
+            self.bib_writer(df=self.df, types=types, alias=alias, dirs=dirs)
             
     def describe(self):
         '''Generates basic metadata'''
