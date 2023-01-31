26711859cb4697d34144accf545d178be947d594
Jong Shin <jshin.m@gmail.com>
2023-01-30 23:50:05 -0500

    Load app details from config file (#29)

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
 from distutils.core import setup
 from setuptools import find_packages
+import yaml
 import pandarize
 
 VERSION = pandarize.__version__
 with open('README.md', 'r') as f:
     README = f.read()
 
+# load from config file
+with yaml.safe_load('config/config.yaml')['app'] as y:
+    APPNAME = y['appname']
+    URL = y['url']
+
 setup(
-    name='Pandarize',
+    name=APPNAME,
     version=VERSION,
     author='Jong M. Shin',
     author_email='jshinm@gmail.com',
     packages=find_packages(),
     package_data = {"": ['pandarize/config/config.yaml']},
     include_package_data=True,
-    url='https://github.com/jshinm/pandarize/',
+    url=URL,
     license='MIT',
     description='Turns data into panda dataframe',
     readme='README.md',
